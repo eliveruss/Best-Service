4d6fec58e3d56b2fb9c5e4aaa454d5db665cf5cc
Aleksander <33355218+eliveruss@users.noreply.github.com>
2022-12-11 02:16:06 +0100

    popup in progress

--- bestservice.js
+++ bestservice.js
@@ -1,5 +1,22 @@
 // Burger menu on mobiles
 
+const burgerbars = document.querySelector(".burgerbars");
+const nav = document.querySelector("nav");
+const services = document.querySelector(".services");
+
+burgerbars.addEventListener("click", () => {
+    burgerbars.classList.toggle("active");
+    nav.classList.toggle("active");
+    services.classList.toggle("active");
+})
+
+nav.addEventListener("click", () => {
+    burgerbars.classList.remove("active");
+    nav.classList.remove("active");
+    services.classList.remove("active");
+})
+
+// Popup modal 
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
